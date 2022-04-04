f3da149d85954a68c8ffff3f4785fdceed0f4022
Mackenzie Nelson <kenzie2397@yahoo.com>
2022-04-03 21:31:05 -0500

    Fix bug attempt 2

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, 
-  ssl: (
+  ssl: {
     rejectUnauthorized: false
-  )
+  }
 });
 
 express()
